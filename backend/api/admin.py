4f81845efaea3a5db9ae7b43303394ec253dc1d1
kivkiv12345 <kevincarlsen@live.dk>
2024-01-29 12:16:50 +0100

    Actions to start/stop servers on admin site

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,13 +1,29 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
+from django.db.models import QuerySet
+from requests import Request
 
 from api.models import GameServer, ServerPermission, ServerEvent
+
+
+@admin.action(description="Start multiple servers")
+def start_servers(modeladmin: ModelAdmin, request: Request, queryset: QuerySet[GameServer]):
+    for server in queryset:
+        server.manager.start()
+
+
+@admin.action(description="Stop multiple servers")
+def stop_servers(modeladmin: ModelAdmin, request: Request, queryset: QuerySet[GameServer]):
+    for server in queryset:
+        server.manager.stop()
 
 
 # Register your models here.
 class GameServerAdmin(ModelAdmin):
     model = GameServer
-    # fields = ['__all__']
+    # fields = *(field.name for field in GameServer._meta.get_fields()),
+    readonly_fields = ['server_version']
+    actions = [start_servers]
 
 
 class ServerPermissionAdmin(ModelAdmin):
