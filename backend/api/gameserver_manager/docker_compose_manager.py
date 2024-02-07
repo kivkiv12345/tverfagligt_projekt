344551563cd02932d91b7968a9a853043de3c857
kivkiv12345 <kevincarlsen@live.dk>
2024-02-07 12:18:45 +0100

    Working on unique port(s) for every server
    It seems that my idea of placing servers on sub-paths doesn't work with TCP/UDP.
    And subdomains are outside the scope of this project.
    So we will probably end up forwarding ports anyway :(

--- backend/api/gameserver_manager/docker_compose_manager.py
+++ backend/api/gameserver_manager/docker_compose_manager.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from os import path
 from os import PathLike
+from pathlib import Path
 
 import yaml
 from abc import ABC
@@ -23,9 +24,11 @@ class AbstractDockerComposeGameServerManager(AbstractGameServerManager, ABC):
     compose_file: str = None
     client: DockerClient
     services: list[str] | str = None  # services must be a list when multiple, because of python-on-whales reasons
+    ports: list[ValidPortMapping] = None
 
     def __init__(self, server_name: str) -> None:
         super().__init__(server_name)
+        self.compose_file = self._mutate_to_proper_compose_file()
         # TODO Kevin: Everything will most likely explode if the server_name/compose_project_name is changed.
         # TODO Kevin: Also, how can we make sure the project name prefix is applied to containers when container_name
         #   is specified in the docker-compose.yml file?
@@ -39,29 +42,39 @@ class AbstractDockerComposeGameServerManager(AbstractGameServerManager, ABC):
     def get_project_name(self):
         return self.server_name.lower().replace(' ', '_')
 
-    @classmethod
-    def _mutate_to_proper_compose_file(cls) -> str:
-        with open(cls.compose_file, 'r') as compose_file:
+    def _mutate_to_proper_compose_file(self) -> str:
+        with open(self.compose_file, 'r') as compose_file:
             compose_contents = yaml.safe_load(compose_file)
 
         assert 'services' in compose_contents, 'How can we have a server, without any docker-compose services'
         # An alternative approach could be:
         #for container in self.client.compose.ps(services=self.services):
         #    container.rename()
+
+        if self.services:
+            for service in set(compose_contents['services'].keys()):
+                if service not in self.services:
+                    del compose_contents['services'][service]
+
         for service in compose_contents['services'].values():
             try:
                 del service['container_name']  # container_name overrules our project_name prefix, so it has to go!
             except KeyError:
                 continue
 
-        assert ' ' not in cls.game_name, "Guess we shouldn't use .game_name after all"
-        mutated_compose_path: str = path.join(COMPOSE_FILES_DIR, f"{cls.game_name}.yml")
+        assert ' ' not in self.game_name, "Guess we shouldn't use .game_name after all"
+        assert '/' not in self.game_name, "Guess we shouldn't use .game_name after all"
+        #assert ' ' not in self.server_name, "Guess we shouldn't use .server_name after all"
+        qual_server_name = self.server_name.replace(' ', '_')
+        assert '/' not in self.server_name, "Guess we shouldn't use .server_name after all"
+        mutated_compose_path: Path = Path(path.join(COMPOSE_FILES_DIR, self.game_name, f"{qual_server_name}.yml"))
+        mutated_compose_path.parent.mkdir(exist_ok=True, parents=True)
         with open(mutated_compose_path, 'w') as mutated_compose_file:
             mutated_compose_file.write(  "# WARNING DO NOT EDIT, YOUR CHANGES WILL BE LOST.\n"
                                          "# THIS FILE HAS BEEN MUTATED FROM THE ORIGINAL\n"
-                                        f"# FOUND HERE: {cls.compose_file}\n\n")
+                                        f"# FOUND HERE: {self.compose_file}\n\n")
             yaml.safe_dump(compose_contents, mutated_compose_file)
-        return mutated_compose_path
+        return str(mutated_compose_path)
 
     def __init_subclass__(cls) -> None:
         """ The only purpose of AbstractDockerComposeGameServerManager.__init_subclass__()
@@ -82,21 +95,12 @@ class AbstractDockerComposeGameServerManager(AbstractGameServerManager, ABC):
         # ._mutate_to_proper_compose_file() creates a compose-file in another directory,
         # but python-on-whales should continue to use the directory of the original compose-file.
         cls.working_directory = path.dirname(cls.compose_file)
-        cls.compose_file = cls._mutate_to_proper_compose_file()
 
     def start(self):
         # TODO Kevin: Container will obviously fail to start,
         #   if the port is already in use by another server.
         # TODO Kevin: .start() or .up()? I dont know.
         self.client.compose.up(detach=True, services=list(self.services))
-        # TODO Kevin: Should we connect to it, or it to us? Does it even matter?
-        for container in self.client.compose.ps(services=self.services):
-            try:
-                # TODO Kevin: Name should not be hardcoded
-                self.client.network.connect("tverfagligt_default", container)
-            except DockerException as e:  # A DockerNetworkError doesn't seem to exist.
-                if 'already exists in network' not in e.stderr:
-                    raise
 
     def stop(self):
         # TODO Kevin: .stop() or .down()? I dont know.
@@ -105,11 +109,14 @@ class AbstractDockerComposeGameServerManager(AbstractGameServerManager, ABC):
     def restart(self):
         self.client.compose.restart()
 
-    def server_running(self) -> bool:
+    def server_running(self) -> bool | None:
         """ Currently returns True if any specified services are running, otherwise False. """
 
         # TODO Kevin: What if the server has multiple services?
-        return bool(self.client.compose.ps(self.services))
+        try:
+            return bool(self.client.compose.ps(self.services))
+        except DockerException:
+            return None
 
     def set_version(self, version: str):
         self.stop()
