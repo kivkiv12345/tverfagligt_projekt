311f49c12627b4a1c5c31915e7875abf9c758859
Gustav Thoft <35370673+TotallyNotRust@users.noreply.github.com>
2024-02-20 15:55:02 +0100

    Changes to how bloc is utilized

--- gameserver_frontend/lib/bloc/server_bloc.dart
+++ gameserver_frontend/lib/bloc/server_bloc.dart
@@ -1,18 +1,42 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gameserver_frontend/ServerListWidget.dart';
 import 'package:gameserver_frontend/api.dart';
 import 'package:gameserver_frontend/bloc/server_event.dart';
 import 'package:gameserver_frontend/bloc/server_state.dart';
 
-class ServerBloc extends Bloc<ServerEvent, ServerState> {
-  ServerBloc(super.state); // Initial state is stopped
+class ServerBloc extends Bloc<ServerEvent, ServerBlocState> {
+  ServerBloc(ServerState state, int statusCode) : super(ServerUnloadedState(state, statusCode)) {
+    on<StartServer>(startServer);
+  }
+
+  Future serverLoaded(ServerLoaded event, Emitter<ServerBlocState> emit) async {
+    List<Server> serverlist = [];
+    emit(ServerLoadedBlocState(serverlist, event.state, 100));
+  }
+
+  Future startServer(
+    StartServer event,
+    Emitter<ServerBlocState> emit,
+  ) async {
+    if (state is ServerLoadedBlocState) {
+      final response = await event.server.start();
+      ServerState _state;
+      if (!bad_statuscode(response.statusCode)) {
+        _state =
+            ServerState.running; // Set state to running if start successful
+      } else {
+        _state =
+            ServerState.stopped; // Set state back to stopped if start failed
+      }
+      emit(state.copyWith());
+    }
+  }
 
   @override
   Stream<ServerState> mapEventToState(ServerEvent event) async* {
-  
     yield ServerState.changing; // Set state to changing while changing...
 
     if (event is StartServer) {
-
       final response = await event.server.start();
 
       if (!bad_statuscode(response.statusCode)) {
@@ -21,7 +45,6 @@ class ServerBloc extends Bloc<ServerEvent, ServerState> {
         yield ServerState.stopped; // Set state back to stopped if start failed
       }
     } else if (event is StopServer) {
-
       final response = await event.server.stop();
 
       if (!bad_statuscode(response.statusCode)) {
